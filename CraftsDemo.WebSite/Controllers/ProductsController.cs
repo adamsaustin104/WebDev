69f26b9e83ac1de5ab2bb6ba430778ec6343b9f5
Austin Adams <adamsaustin104@gmail.com>
2020-02-06 19:49:41 -0700

    CraftsDemo - Recommiting finished project
    Had to delete old repository.  Uploaded finished project to new repo

---
+++ CraftsDemo.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using CraftsDemo.WebSite.Models;
+using CraftsDemo.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftsDemo.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ CraftsDemo.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using CraftsDemo.WebSite.Models;
+using CraftsDemo.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CraftsDemo.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService _jsonFileProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            _jsonFileProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = _jsonFileProductService.GetProducts();
+        }
+    }
+}
